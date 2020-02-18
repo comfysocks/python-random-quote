e9007ad1c5a2ec4bab417eecc41c1f9c3651abee
comfysocks <57093926+comfysocks@users.noreply.github.com>
2020-02-18 11:35:49 +0100

    Update and rename Renamed the primary function.py to Random quote bot is working.py

---
+++ Random quote bot is working.py
@@ -0,0 +1,16 @@
+import random
+
+def unoo():
+  print("Keep it logically awesome.")
+
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+
+  last = 13
+  rnd = random.randint(0, last)
+  
+  print(quotes[rnd])
+
+if __name__== "__main__":
+  unoo()
